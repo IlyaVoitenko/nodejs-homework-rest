b485f3985fd71ea9ecd856a801021a2a52e0c5e2
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-05-17 12:33:46 +0300

    added mongoose and process.env

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const { HOST_DB } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(HOST_DB)
+  .then(() => app.listen(3000))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
